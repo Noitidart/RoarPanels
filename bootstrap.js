4b8fa42dd2e8cf168cf12653fba93a267aa3a2ff
Noitidart <noitidart@gmail.com>
2014-10-23 00:57:28 -0700

    initial.rev1
    throwPop works

--- bootstrap.js
+++ bootstrap.js
@@ -4,16 +4,27 @@ const self = {
 	suffix: '@jetpack',
 	id: 'RoarPanels@jetpack',
 	path: 'chrome://roarpanels/content/',
-	aData: 0,
+	aData: 0
 };
 
 const myServices = {};
+Cu.import('resource://gre/modules/devtools/Console.jsm');
 Cu.import('resource://gre/modules/Services.jsm');
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 XPCOMUtils.defineLazyGetter(myServices, 'as', function(){ return Cc['@mozilla.org/alerts-service;1'].getService(Ci.nsIAlertsService) });
 
+var cActPops = []; //holds obj of {top:0, height:0} of the panels currently thrown //currentlyActivePopups
+
+function throwPop(msg, type) {
+	var msgIsHowManyRows = 3;
+	var heightOfTextRow = 14;
+	var heightOfDiv = 78 + ((msgIsHowManyRows-1) * heightOfTextRow)
+	Services.ww.openWindow(null, self.path + 'roar.xul', '_blank', 'chrome,alwaysRaised,width=315,height=' + heightOfDiv, null);
+}
+
 function startup(aData, aReason) {
 	self.aData = aData;
+	console.log('started');
 }
 
 function shutdown(aData, aReason) {
